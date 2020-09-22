0513792e4c532380d4862feeb9ef43305da8a6a6
Crowdbotics <team@crowdbotics.com>
2020-09-22 17:02:05 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/dragon_distillery_20569/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dragon_distillery_20569;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "dragon_distillery_20569";
+    }
+}
